7e99c669b36757c1c0eb833774b3511de2ca68a8
Matheus Mota <matheuzwork@gmail.com>
2024-04-08 11:16:30 -0300

    Update achar_maior.c

--- achar_maior.c
+++ achar_maior.c
@@ -7,8 +7,8 @@
 int indice_maior_num_lista(int *lista, int tamanho) {
     int maior = lista[0];
     int indice = 0;
-    
-    for (int i = 1; i < tamanho; i++) {
+    int i = 1;
+    for (i; i < tamanho; i++) {
         if (lista[i] > maior) {
             indice = i;
             maior = lista[i];
@@ -21,29 +21,32 @@ int indice_maior_num_lista(int *lista, int tamanho) {
 int main() {
     int valor = 0;
     int *dados_sw = &valor;
-    int lista[] = {8, 2, 3, 4, 1};
+    int lista[] = {0,1,5,9,1};
     int tamanho = sizeof(lista) / sizeof(lista[0]);
     int indice_maior = indice_maior_num_lista(lista, tamanho);
 
     printf("Elementos da lista:\n");
-    for (int i = 0; i < tamanho; i++) {
+    int i = 0;
+    for (i; i < tamanho; i++) {
         printf("%d\n", lista[i]);
+
     }
 
     printf("Índice do maior elemento da lista: %d\n", indice_maior);
-
     SW_open();
+    LEDR_open();
 
     while (1) {
+        SW_read(dados_sw);
         if (*dados_sw == 1) {
-            LEDR_open();
             LEDR_set(indice_maior);
-            SW_read(dados_sw);
         }
+        else{
+            LEDR_set(0);
+        } 
     }
-
-    LEDR_close();
-    SW_close();
-
-    return 0;
+        LEDR_close();
+        SW_close();
+   return 0;
 }
+
