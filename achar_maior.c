124b92fef88d13290e233d12c647022a3299dd05
Matheus Mota <matheuzwork@gmail.com>
2024-04-08 09:47:50 -0300

    feat: dois

--- achar_maior.c
+++ achar_maior.c
@@ -1,17 +1,49 @@
+#include <intelfpgaup/LEDR.h>
+#include <intelfpgaup/SW.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+int indice_maior_num_lista(int *lista, int tamanho) {
+    int maior = lista[0];
+    int indice = 0;
+    
+    for (int i = 1; i < tamanho; i++) {
+        if (lista[i] > maior) {
+            indice = i;
+            maior = lista[i];
+        }
+    }
+    
+    return indice;
+}
 
-int main(void) {
-  int arr[] = {8,7,9,2,1};
-  int *ptr = arr;
+int main() {
+    int valor = 0;
+    int *dados_sw = &valor;
+    int lista[] = {8, 2, 3, 4, 1};
+    int tamanho = sizeof(lista) / sizeof(lista[0]);
+    int indice_maior = indice_maior_num_lista(lista, tamanho);
 
-  int maior_valor = arr[0];
-  for(int i = 0; i < 5; i++){
-    if(arr[i] > maior_valor){
-      maior_valor = arr[i];
-      };
-  }
-  printf("%d ", maior_valor);
+    printf("Elementos da lista:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d\n", lista[i]);
+    }
 
-  
-};
+    printf("Índice do maior elemento da lista: %d\n", indice_maior);
+
+    SW_open();
+
+    while (1) {
+        if (*dados_sw == 1) {
+            LEDR_open();
+            LEDR_set(indice_maior);
+            SW_read(dados_sw);
+        }
+    }
+
+    LEDR_close();
+    SW_close();
+
+    return 0;
+}
